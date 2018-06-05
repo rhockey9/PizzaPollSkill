bc2b56d3bd3538e37208bd30cc7b94d471442756
Craig Stephans <cpstephans@gmail.com>
2018-06-04 23:21:57 -0400

    Initial add

---
+++ PizzaPollSkill/PizzaPolllFunction.cs
@@ -0,0 +1,95 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Alexa.NET.Request;
+using Alexa.NET.Request.Type;
+using Alexa.NET.Response;
+using Amazon.Lambda.Core;
+using Newtonsoft.Json;
+
+// Assembly attribute to enable the Lambda function's JSON input to be converted into a .NET class.
+[assembly: LambdaSerializer(typeof(Amazon.Lambda.Serialization.Json.JsonSerializer))]
+
+namespace PizzaPollSkill
+{
+    public class PizzaPollFunction
+    {
+        
+        /// <summary>
+        /// A simple function that takes a string and does a ToUpper
+        /// </summary>
+        /// <param name="input"></param>
+        /// <param name="context"></param>
+        /// <returns></returns>
+        public SkillResponse FunctionHandler(SkillRequest input, ILambdaContext context)
+        {
+            SkillResponse response = new SkillResponse();
+            response.Response = new ResponseBody();
+            response.Response.ShouldEndSession = false;
+            IOutputSpeech innerResponse = null;
+            var log = context.Logger;
+            log.LogLine($"Skill Request Object:");
+            log.LogLine(JsonConvert.SerializeObject(input));
+
+            if (input.GetRequestType() == typeof(LaunchRequest))
+            {
+                log.LogLine($"Default LaunchRequest made: 'Alexa, open Science Facts");
+                innerResponse = new PlainTextOutputSpeech();
+                (innerResponse as PlainTextOutputSpeech).Text = "Welcome to Pizza Poll Skill";
+            }
+            else if (input.GetRequestType() == typeof(IntentRequest))
+            {
+                var intentRequest = (IntentRequest)input.Request;
+
+                switch (intentRequest.Intent.Name)
+                {
+                    case "AMAZON.CancelIntent":
+                        log.LogLine($"AMAZON.CancelIntent: send StopMessage");
+                        innerResponse = new PlainTextOutputSpeech();
+                        (innerResponse as PlainTextOutputSpeech).Text = "Goodbye";
+                        response.Response.ShouldEndSession = true;
+                        break;
+                    case "AMAZON.StopIntent":
+                        log.LogLine($"AMAZON.StopIntent: send StopMessage");
+                        innerResponse = new PlainTextOutputSpeech();
+                        (innerResponse as PlainTextOutputSpeech).Text = "Goodbye";
+                        response.Response.ShouldEndSession = true;
+                        break;
+                    case "AMAZON.HelpIntent":
+                        log.LogLine($"AMAZON.HelpIntent: send HelpMessage");
+                        innerResponse = new PlainTextOutputSpeech();
+                        (innerResponse as PlainTextOutputSpeech).Text = "You can say tell me a favorite pizza topping, or, you can say exit... What can I help you with?";
+                        break;
+                    case "GetPizzaMeatToppingIntent":
+                        log.LogLine($"GetPizzaMeatToppingIntent sent: send meat pizza topping");
+                        innerResponse = new PlainTextOutputSpeech();
+                        (innerResponse as PlainTextOutputSpeech).Text = "Pepperoni";
+                        break;
+                    case "GetPizzaVeggieToppingIntent":
+                        log.LogLine($"GetPizzaVeggieToppingIntent sent: send veggie pizza topping");
+                        innerResponse = new PlainTextOutputSpeech();
+                        (innerResponse as PlainTextOutputSpeech).Text = "Pineapple";
+                        break;
+                    case "GetPizzaSideIntent":
+                        log.LogLine($"GetPizzaSideIntent sent: send pizza side");
+                        innerResponse = new PlainTextOutputSpeech();
+                        (innerResponse as PlainTextOutputSpeech).Text = "Breadsticks";
+                        break;
+                    default:
+                        log.LogLine($"Unknown intent: " + intentRequest.Intent.Name);
+                        innerResponse = new PlainTextOutputSpeech();
+                        (innerResponse as PlainTextOutputSpeech).Text = "You can tell me your favorite pizza topping";
+                        break;
+                }
+            }
+
+            response.Response.OutputSpeech = innerResponse;
+            response.Version = "1.0";
+            log.LogLine($"Skill Response Object...");
+            log.LogLine(JsonConvert.SerializeObject(response));
+
+            return response;
+        }
+    }
+}
